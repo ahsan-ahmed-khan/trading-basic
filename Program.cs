a9a9cf921a2374c76f618c9580902036a82ad089
dimsysdev <adil.rajput@dimensionalsys.com>
2023-08-19 16:47:18 +0500

    create basics

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("OHLC");
+Console.WriteLine("open hua hai 20");
+Console.WriteLine("12 pm tak 30 ");
+Console.WriteLine("2 pm 15");
+Console.WriteLine("closing 25");
